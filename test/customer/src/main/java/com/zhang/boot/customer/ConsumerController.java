280fce4716c30874bc17980fdb1dda4d12a5b0aa
zhanglijun <zhanglijun5@xiaomi.com>
2017-03-25 18:59:11 +0800

    Initial commit

---
+++ test/customer/src/main/java/com/zhang/boot/customer/ConsumerController.java
@@ -0,0 +1,24 @@
+package com.zhang.boot.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ * Created by zhanglijun on 17-3-25.
+ */
+@RestController
+public class ConsumerController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public @ResponseBody String add() {
+        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+    }
+
+}
---
+++ test/server/src/main/java/com/zhang/boot/Application.java
@@ -0,0 +1,16 @@
+package com.zhang.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by zhanglijun on 17-3-25.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
