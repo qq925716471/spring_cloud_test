47bf22df1297497bdb9e677bf62f136f2bd65324
qq925716471 <925716471@qq.com>
2017-03-26 18:16:27 +0800

    config_server

--- customer/src/main/java/com/zhang/boot/customer/ConsumerController.java
+++ customer/src/main/java/com/zhang/boot/customer/ConsumerController.java
@@ -2,6 +2,8 @@ package com.zhang.boot.customer;
 
 import com.zhang.boot.customer.service.ComputeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +14,20 @@ import org.springframework.web.client.RestTemplate;
 /**
  * Created by zhanglijun on 17-3-25.
  */
+@RefreshScope
 @RestController
 public class ConsumerController {
     @Autowired
     RestTemplate restTemplate;
     @Autowired
     ComputeService computeService;
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add() {
