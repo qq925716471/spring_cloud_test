7fb70b27a619161ae408a727d1f74967bd367540
zhanglijun <zhanglijun5@xiaomi.com>
2017-03-27 20:42:21 +0800

    gateway

--- test/gate_way/src/main/java/com/zhang/boot/GateWayApplication.java
+++ test/gate_way/src/main/java/com/zhang/boot/GateWayApplication.java
@@ -1,8 +1,10 @@
 package com.zhang.boot;
 
+import com.zhang.boot.filter.AccessFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by zhanglijun on 17-3-27.
@@ -12,5 +14,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class GateWayApplication {
     public static void main(String[] args){
         new SpringApplicationBuilder(GateWayApplication.class).web(true).run(args);
+    }
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
